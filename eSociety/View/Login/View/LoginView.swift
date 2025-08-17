934df68bec22f7f701f91ff3a483d36457dcbb48
Mahaboobsab <mrnadaf2010@gmail.com>
2025-08-17 23:55:15 +0530

    Added Scroll support

--- eSociety/View/Login/View/LoginView.swift
+++ eSociety/View/Login/View/LoginView.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Alkit Gupta on 17/08/25.
 //
-
 import SwiftUI
 import DesignColors
 import CoreSwift
@@ -16,47 +15,45 @@ struct LoginView: View {
     var body: some View {
         ZStack {
             DesignColors.softWhite.ignoresSafeArea()
-            VStack {
-                VStack(spacing: 20) {
-                    Text(kesociety.localized)
-                        .font(AppFont.robotoBold(size: 50))
-                    
-                    Text(kuserLogin.localized)
-                        .font(AppFont.robotoBold(size: 30))
-                }
-                .padding(.top, 50)
-                .padding()
-                VStack(spacing: 16) {
-                    TextField(kemailID.localized, text: $viewModel.email)
-                        .keyboardType(.emailAddress)
-                        .autocapitalization(.none)
-                        .textContentType(.emailAddress)
-                        .inputFieldStyle()
-                    SecureField(kpassword.localized, text: $viewModel.password)
-                        .textContentType(.password)
-                        .inputFieldStyle()
-                }
-                .padding(.horizontal)
-                
-                VStack(spacing: 20) {
-                    Button(klogin.localized) {
-                        // Handle login
+            
+            ScrollView {
+                VStack(spacing: 32) {
+                    VStack(spacing: 20) {
+                        Text(kesociety.localized)
+                            .font(AppFont.robotoBold(size: 50))
+                        
+                        Text(kuserLogin.localized)
+                            .font(AppFont.robotoBold(size: 30))
                     }
-                    .disabled(!viewModel.isFormValid)
-                    .primaryButtonStyle(enabled: viewModel.isFormValid)
+                    .padding(.top, 50)
+                    .padding(.horizontal)
+                    
+                    VStack(spacing: 16) {
+                        TextField(kemailID.localized, text: $viewModel.email)
+                            .keyboardType(.emailAddress)
+                            .autocapitalization(.none)
+                            .textContentType(.emailAddress)
+                            .inputFieldStyle()
+                        
+                        SecureField(kpassword.localized, text: $viewModel.password)
+                            .textContentType(.password)
+                            .inputFieldStyle()
+                    }
+                    .padding(.horizontal)
+                    
+                    VStack(spacing: 20) {
+                        Button(klogin.localized) {
+                            // Handle login
+                        }
+                        .disabled(!viewModel.isFormValid)
+                        .primaryButtonStyle(enabled: viewModel.isFormValid)
+                    }
+                    .padding(.horizontal)
+                    
+                    Spacer(minLength: 50)
                 }
-                .padding()
-                Spacer()
+                .frame(maxWidth: .infinity)
             }
         }
     }
 }
-
-
-struct LoginView_Previews: PreviewProvider {
-    static var previews: some View {
-        LoginView()
-    }
-}
-
-
