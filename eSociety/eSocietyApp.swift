b01bcf5170c0c63964b68c947bf16249e5a11c81
Mahaboobsab <mrnadaf2010@gmail.com>
2025-08-17 19:45:29 +0530

    Initial Commit

---
+++ eSociety/eSocietyApp.swift
@@ -0,0 +1,17 @@
+//
+//  eSocietyApp.swift
+//  eSociety
+//
+//  Created by Alkit Gupta on 17/08/25.
+//
+
+import SwiftUI
+
+@main
+struct eSocietyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
